d1e90ad87a37fb911095c788551f4fbb3737d3fd
Lukasz Malinowski <lukasz.malinowski10@gmail.com>
2021-11-18 21:14:01 +0100

    Refactored exception handlers

--- src/main/java/com/herokuapp/trello_inspired_app/trelloinspiredappbackend/rest/handlers/BoardExceptionHandler.java
+++ src/main/java/com/herokuapp/trello_inspired_app/trelloinspiredappbackend/rest/handlers/BoardExceptionHandler.java
@@ -1,21 +1,19 @@
 package com.herokuapp.trello_inspired_app.trelloinspiredappbackend.rest.handlers;
 
 import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.dto.ExceptionDto;
+import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.BaseExceptionHandler;
 import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.BoardNotFoundException;
-import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.GlobalExceptionHandler;
-import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.UserAlreadyHasAdminException;
-import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.UserIsNotMemberOfBoardException;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Locale;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-@RestControllerAdvice
-public class BoardExceptionHandler extends GlobalExceptionHandler {
+@ControllerAdvice
+public class BoardExceptionHandler extends BaseExceptionHandler {
 
     public BoardExceptionHandler(MessageSource messageSource) {
         super(messageSource);
@@ -24,17 +22,6 @@ public class BoardExceptionHandler extends GlobalExceptionHandler {
     @ExceptionHandler (BoardNotFoundException.class)
     ResponseEntity<ExceptionDto> onBoardNotFoundException(BoardNotFoundException exception, Locale locale) {
         return createResponse(exception, NOT_FOUND, locale);
-    }
-
-    @ExceptionHandler (UserAlreadyHasAdminException.class)
-    ResponseEntity<ExceptionDto> onUserAlreadyHasAdminException(UserAlreadyHasAdminException exception, Locale locale) {
-        return createResponse(exception, CONFLICT, locale);
-    }
-
-    @ExceptionHandler (UserIsNotMemberOfBoardException.class)
-    ResponseEntity<ExceptionDto> onUserIsNotMemberOfBoardException(UserIsNotMemberOfBoardException exception,
-                                                                   Locale locale) {
-        return createResponse(exception, BAD_REQUEST, locale);
     }
 
 }
--- src/main/java/com/herokuapp/trello_inspired_app/trelloinspiredappbackend/rest/handlers/GlobalExceptionHandler.java
+++ src/main/java/com/herokuapp/trello_inspired_app/trelloinspiredappbackend/rest/handlers/GlobalExceptionHandler.java
@@ -1,33 +1,31 @@
-package com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception;
+package com.herokuapp.trello_inspired_app.trelloinspiredappbackend.rest.handlers;
 
 import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.dto.ExceptionDto;
 import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.dto.ValidationExceptionDto;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import com.herokuapp.trello_inspired_app.trelloinspiredappbackend.exception.BaseExceptionHandler;
 import org.springframework.context.MessageSource;
-import org.springframework.context.NoSuchMessageException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 
-import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+@ControllerAdvice
+public class GlobalExceptionHandler extends BaseExceptionHandler {
 
-@Slf4j
-@RequiredArgsConstructor
-public class GlobalExceptionHandler {
-
-    private final MessageSource messageSource;
-
-    @ExceptionHandler (Exception.class)
-    protected ResponseEntity<ExceptionDto> onException(Exception exception, Locale locale) {
-        exception.printStackTrace();
-        return createResponse(exception, INTERNAL_SERVER_ERROR, locale);
+    public GlobalExceptionHandler(MessageSource messageSource) {
+        super(messageSource);
     }
+
+    //TODO: figure out how to fix it
+//    @ExceptionHandler (Exception.class)
+//    protected ResponseEntity<ExceptionDto> onException(Exception exception, Locale locale) {
+//        exception.printStackTrace();
+//        return createResponse(exception, INTERNAL_SERVER_ERROR, locale);
+//    }
 
     @ExceptionHandler (MethodArgumentNotValidException.class)
     protected ResponseEntity<ExceptionDto> onValidationException(MethodArgumentNotValidException exception,
@@ -38,28 +36,11 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest().body(new ValidationExceptionDto(description, validationErrors));
     }
 
-    protected ResponseEntity<ExceptionDto> createResponse(Exception exception, HttpStatus status, Locale locale) {
-        String description = getDescription(exception, locale);
-        return ResponseEntity.status(status).body(new ExceptionDto(description));
-    }
-
     private Map<String, String> getValidationErrors(MethodArgumentNotValidException exception) {
         Map<String, String> validationErrors = new HashMap<>();
         exception.getBindingResult().getFieldErrors()
                  .forEach(error -> validationErrors.put(error.getField(), error.getDefaultMessage()));
         return validationErrors;
-    }
-
-    private String getDescription(Exception exception, Locale locale) {
-        var key = exception.getClass().getSimpleName();
-        String description;
-        try {
-            description = messageSource.getMessage(key, new Object[]{"test"}, locale);
-        }
-        catch (NoSuchMessageException ex) {
-            description = key;
-        }
-        return description;
     }
 
 }
